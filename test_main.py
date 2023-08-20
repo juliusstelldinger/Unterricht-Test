bba0610133313ac14f81be6761b08300a295f53a
SirKaktor <juliuststelldinger@web.de>
2023-08-20 10:14:04 +0200

    changed according to flake

--- test_main.py
+++ test_main.py
@@ -3,15 +3,16 @@ import unittest
 from main import quadrizzle
 
 class TestQuadrizzle(unittest.TestCase):
-    
+
+
     def test_two_squared_is_four(self):
         # arrange
-        
+
         # act
         result = quadrizzle(2)
-        
+
         # assert
         self.assertEqual(result, 4)
-        
+
 if __name__ == "__main__":
     unittest.main()
